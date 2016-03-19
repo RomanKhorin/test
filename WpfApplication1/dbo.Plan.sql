8b4d4325dd09c1e3eaaf4247977f7f69cae87acf
RomanKhorin <rhorin@yandex.ru>
2016-03-19 14:51:12 +0300

    m,mn,mn

---
+++ WpfApplication1/dbo.Plan.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Plan] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [User_login]  VARCHAR (50)  NOT NULL,
+    [Description] VARCHAR (140) NOT NULL,
+    [IsCompleted] VARCHAR (1)   NOT NULL,
+    [DeadLine]    DATE          NOT NULL,
+    [Date]        DATETIME      NOT NULL,
+    CONSTRAINT [PK_Plan] PRIMARY KEY CLUSTERED ([Date] ASC, [Id] ASC),
+    CONSTRAINT [User_login] FOREIGN KEY ([User_login]) REFERENCES [dbo].[User] ([Login])
+);
+
